8a9850945c4871379e1109ec7d4c12327f237ab4
Paras Sahu <paras2115s@gmail.com>
2026-07-29 12:34:50 +0530

    reverse

--- app.py
+++ app.py
@@ -217,16 +217,16 @@ def predict():
 
 
 
-   except Exception:
-    import traceback
-
-    traceback.print_exc()
+    except Exception as e:
 
 
-    return jsonify({
-        "success": False,
-        "error": traceback.format_exc()
-    }), 500
+        return jsonify({
+
+            "success":False,
+
+            "error":str(e)
+
+        }),500
 
 
 
